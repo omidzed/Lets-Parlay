abfd743e7c9f8b3eb96125f3110c8c2210d3f03f
Omid Asadi <omiddzedd@gmail.com>
2024-01-23 18:00:31 +0000

    updated vite and some configurations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,9 @@ import { NavBar } from './components/NavBar';
 
 export default function App() {
   return (
-    <>
+    <div>
       <NavBar />
       <EventsCarousel />
-    </>
+    </div>
   );
 }
