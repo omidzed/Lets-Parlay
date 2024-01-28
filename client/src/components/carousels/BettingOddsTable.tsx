bcfb76ecd8352ff475644e3bb5da05b3ebdbcfec
Omid Asadi <omiddzedd@gmail.com>
2024-01-28 20:48:55 +0000

    created fisrt partr of bet table

--- client/src/components/carousels/BettingOddsTable.tsx
+++ client/src/components/carousels/BettingOddsTable.tsx
@@ -1,3 +1,28 @@
 export function BettingOddsTable() {
-  return <></>;
+  // const styling = '';
+
+  return (
+    <div className="flex justify-center">
+      <div className="rounded-md bg-zinc-800 w-[90%] h-28 mt-4">
+        <div></div>
+        <div>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
