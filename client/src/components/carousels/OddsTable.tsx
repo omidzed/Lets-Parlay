8e012e4f5242d075a0799980106641b17ca2b015
Omid Asadi <omiddzedd@gmail.com>
2024-01-31 07:28:26 +0000

    updated packagejson and rewrote Date object

--- client/src/components/carousels/OddsTable.tsx
+++ client/src/components/carousels/OddsTable.tsx
@@ -6,14 +6,13 @@ type Props = {
 
 export function OddsTable({ events }: Props) {
   const formatDateTime = (dateTimeString: Date) => {
-    // const options = {
-    //   year: 'numeric',
-    //   month: '2-digit',
-    //   day: '2-digit',
-    //   hour: '2-digit',
-    //   minute: '2-digit',
-    // };
-    return new Date(dateTimeString).toLocaleDateString();
+    return new Date(dateTimeString).toLocaleDateString('default', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    });
   };
 
   const boxStyling =
