03383e845697ab11fc68a502e996bf697da76dd8
Omid Asadi <omiddzedd@gmail.com>
2024-02-07 18:17:51 +0000

    began work on history page

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,3 +1,19 @@
-export function History() {
-  return <div></div>;
-}
+// export function History() {
+//   type Bets = {
+//     betId: number;
+//     eventId: string;
+//     betType: string;
+//     betAmount: number;
+//   };
+
+//   type Outcome = {
+//     betId: number;
+//     eventId: string;
+//     betType: string;
+//     betAmount: number;
+//     winLose: string;
+//     payout: number;
+//   };
+
+//   return <div></div>;
+// }
