0088efa2b941e000853a23f93c1605bcb85c73cc
Omid Asadi <omiddzedd@gmail.com>
2024-04-02 05:19:53 +0000

    updated carousel

--- client/src/components/carousels/EventsCarousel.tsx
+++ client/src/components/carousels/EventsCarousel.tsx
@@ -2,7 +2,9 @@ import { RotatingBanner } from './RotatingBanner';
 
 export const EventsCarousel = () => {
   const posters = [
+    '/images/2024-04-06.png',
     '/images/2024-04-13.png',
+    '/images/2024-04-27.png',
     '/images/2024-05-11.png',
     '/images/2024-05-18.png',
     '/images/2024-06-22.png',
