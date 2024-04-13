f259fae23e08239cccb9137b73431c8c22a7155c
Omid Asadi <omiddzedd@gmail.com>
2024-04-13 19:46:25 +0000

    implemented a serve project for letsParlay on vercel

--- server/server.ts
+++ server/server.ts
@@ -191,6 +191,10 @@ app.use(defaultMiddleware(reactStaticDir));
 
 app.use(errorMiddleware);
 
+app.use('/', (req, res) => {
+  res.send('Server is running');
+});
+
 app.listen(process.env.PORT, () => {
   process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
 });
