94794ed2d563f2a4a6270e1cbcca87370e7ea6a4
Omid Asadi <omiddzedd@gmail.com>
2024-04-29 00:48:16 +0000

    code cleanup

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,8 +1,4 @@
 import { useState } from 'react';
-import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
-} from '../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
   displayName: '',
