5b97416884ca1eec50377f6f5b18253351a535ae
Omid Asadi <omiddzedd@gmail.com>
2024-09-05 19:14:47 +0000

    updated gitHub actions and posters

--- client/src/components/carousel/EventsCarousel.tsx
+++ client/src/components/carousel/EventsCarousel.tsx
@@ -1,7 +1,11 @@
 import { RotatingBanner } from './RotatingBanner';
 
 export const EventsCarousel = () => {
-  const posters = ['images/2024-09-07.png', 'images/2024-09-14.png'];
+  const posters = [
+    'images/2024-09-07.png',
+    'images/2024-09-14.png',
+    'images/2024-09-28.png',
+  ];
 
   return (
     <div className="mt-2">
