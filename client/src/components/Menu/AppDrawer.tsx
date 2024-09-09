9e0f39fa2504840376c7122bb2cc6b5671405e57
Omid Asadi <omiddzedd@gmail.com>
2024-09-09 16:56:20 +0000

    Menu styling changed

--- client/src/components/Menu/AppDrawer.tsx
+++ client/src/components/Menu/AppDrawer.tsx
@@ -62,17 +62,17 @@ export const AppDrawer = ({
     <div
       className={`fixed top-0 left-0 h-screen z-50 transition-transform duration-700 ease-in-out bg-[#141414] ${
         isOpen ? 'translate-x-0' : '-translate-x-full'
-      } w-[87%] sm:w-[60%] md:w-[40%] lg:w-[30%] xl:w-[25%] 2xl:w-[17%]`}>
+      } w-[90%] sm:w-[60%] md:w-[40%] lg:w-[30%] xl:w-[25%] 2xl:w-[17%]`}>
       <div className={'drawer-container'}>
         <div className={isOpen ? 'hidden' : 'hamburger-container'}></div>
         <div className={isOpen ? 'menu-drawer open' : 'menu-drawer closed'}>
           <div className="flex justify-center ">
-            <div className="fixed top-12 right-4 md:right-6 md:top-16 cursor-pointer">
+            <div className="fixed top-12 right-6 md:top-16 cursor-pointer">
               <IoClose
                 onClick={() => toggleMenu()}
                 color="white"
                 size={45}
-                className="md:ml-5  p-3 md:p-2 cursor-pointer hover:border-2 hover:scale-90 duration-200 hover:border-yellow-400 hover:bg-black rounded-full transition ease-out"
+                className="md:ml-5 p-2 cursor-pointer hover:border-2 hover:scale-90 duration-200 hover:border-yellow-400 hover:bg-black rounded-full transition ease-out"
               />
             </div>
             <div className="mt-14 flex flex-col justify-center items-center">
