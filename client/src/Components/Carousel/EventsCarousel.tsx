803ce5bd4233ca0fdaa84f130686f00748cc4ef2
Omid Asadi <omiddzedd@gmail.com>
2025-02-04 06:14:10 +0000

    posters update

--- client/src/Components/Carousel/EventsCarousel.tsx
+++ client/src/Components/Carousel/EventsCarousel.tsx
@@ -1,7 +1,11 @@
 import { RotatingBanner } from './RotatingBanner';
 
 export const EventsCarousel = () => {
-  const posters = ['images/2-8-25.png', 'images/2-15-25.png'];
+  const posters = [
+    'images/2-8-25.png',
+    'images/2-15-25.png',
+    'images/2-22-25.png',
+  ];
 
   return (
     <div className="mt-2">
