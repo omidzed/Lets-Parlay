20e542ee89d83f3ead560df2f3a2a35cbe0a3dcb
Omid Asadi <omid.develops@gmail.com>
2025-06-11 15:54:22 +0000

    updated odds generator

--- client/src/utils/generate-random-odds.ts
+++ client/src/utils/generate-random-odds.ts
@@ -1,5 +1,5 @@
 export const generateRealisticOverUnderOdds = (isOver: boolean): number => {
-  const oddsArray = [-250, -200, -175, -150, -110, 110, 150, 200, 250, 400, 500];
+  const oddsArray = [-250, -200, -175, -150, -110, 110, 150, 200, 250, 300, 400, 500];
 
   const randomIndex = Math.floor(Math.random() * oddsArray.length);
   return oddsArray[randomIndex];
