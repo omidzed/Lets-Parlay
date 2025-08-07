54c254a69bc875b09f7a205eee54bf5c69064756
Omid Asadi <omid.develops@gmail.com>
2025-08-07 10:48:03 +0000

    ufc 319 poster, new Supabase db added

--- client/src/Components/Carousel/EventsCarousel.tsx
+++ client/src/Components/Carousel/EventsCarousel.tsx
@@ -1,7 +1,7 @@
 import { RotatingBanner } from './RotatingBanner';
 
 export const EventsCarousel = () => {
-  const posters = ['images/6-21-25.png', 'images/6-28-25.png'];
+  const posters = ['images/8-16-25.png'];
 
   return (
     <div className="mt-2">
